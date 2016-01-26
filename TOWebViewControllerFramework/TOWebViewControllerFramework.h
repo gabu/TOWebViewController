08e185e38825ac4c3da9798da124e3e62772c943
gabu <tsukada.shouya@gmail.com>
2016-01-26 17:08:43 +0900

    Added framework target

---
+++ TOWebViewControllerFramework/TOWebViewControllerFramework.h
@@ -0,0 +1,19 @@
+//
+//  TOWebViewControllerFramework.h
+//  TOWebViewControllerFramework
+//
+//  Created by Shoya Tsukada on 2016/01/26.
+//  Copyright © 2016年 Tim Oliver. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TOWebViewControllerFramework.
+FOUNDATION_EXPORT double TOWebViewControllerFrameworkVersionNumber;
+
+//! Project version string for TOWebViewControllerFramework.
+FOUNDATION_EXPORT const unsigned char TOWebViewControllerFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TOWebViewControllerFramework/PublicHeader.h>
+
+
